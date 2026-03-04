1d14fdfc5e62d9849dd3c5eb6de0bfd645ab64cd
sortupdev5 <sortup.dev5@gmail.com>
2026-03-04 22:57:33 +0500

    fix(vercel): inline route adapter to bypass resolution issues

--- apps/erp/app/routes.ts
+++ apps/erp/app/routes.ts
@@ -1,6 +1,6 @@
 import type { RouteConfig } from "@react-router/dev/routes";
-import { remixRoutesOptionAdapter } from "@react-router/remix-routes-option-adapter";
 import { flatRoutes } from "remix-flat-routes";
+import { remixRoutesOptionAdapter } from "./route-adapter";
 
 export default remixRoutesOptionAdapter((defineRoutes) => {
   return flatRoutes("routes", defineRoutes, {
